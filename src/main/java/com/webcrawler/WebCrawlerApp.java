eb169370e3d1e6eb85a783877cb65e7abe08e8cd
lars1son <artyom_k_97@mail.ru>
2019-01-30 19:07:40 +0200

    clear code, add info to readme

--- src/main/java/com/webcrawler/WebCrawlerApp.java
+++ src/main/java/com/webcrawler/WebCrawlerApp.java
@@ -27,52 +27,5 @@ public class WebCrawlerApp extends JFrame {
         crawlerFrame.setVisible(true);
         log.info("==================STARTED==================");
     }
-//    public WebCrawlerApp() {
-//
-//        initUI();
-//    }
-//
-//    private void initUI() {
-//
-//        JButton quitButton = new JButton("Quit");
-//
-//        quitButton.addActionListener((ActionEvent event) -> {
-//            System.exit(0);
-//        });
-//
-//        createLayout(quitButton);
-//
-//        setTitle("Quit button");
-//        setSize(300, 200);
-//        setLocationRelativeTo(null);
-//        setDefaultCloseOperation(EXIT_ON_CLOSE);
-//    }
-//
-//    private void createLayout(JComponent... arg) {
-//
-//        Container pane = getContentPane();
-//        GroupLayout gl = new GroupLayout(pane);
-//        pane.setLayout(gl);
-//
-//        gl.setAutoCreateContainerGaps(true);
-//
-//        gl.setHorizontalGroup(gl.createSequentialGroup()
-//                .addComponent(arg[0])
-//        );
-//
-//        gl.setVerticalGroup(gl.createSequentialGroup()
-//                .addComponent(arg[0])
-//        );
-//    }
-//
-//    public static void main(String[] args) {
-//
-//        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(WebCrawlerApp.class)
-//                .headless(false).run(args);
-//
-//        EventQueue.invokeLater(() -> {
-//            WebCrawlerApp ex = ctx.getBean(WebCrawlerApp.class);
-//            ex.setVisible(true);
-//        });
-//    }
+
 }
